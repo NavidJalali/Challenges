d20a28689e04d51148e0143ef97bca92e3bb5e19
NavidJalali <nvd.eth0@gmail.com>
2022-04-16 03:18:12 +0200

    🔧 update explanation

--- src/io/navidjalali/FindTheDuplicateNumber.java
+++ src/io/navidjalali/FindTheDuplicateNumber.java
@@ -11,6 +11,23 @@ Approach: Since the array contains n + 1 numbers and all lie between 1 to n incl
 We are looking for the first point where a cycle starts.
 Floyd's linked list cycle finder: For this you can use a slow and fast pointer. Once they meet you start another
 slow pointer from the head and move both pointers at the same pace. They will meet at the start of the cycle.
+
+Proof:
+ <---- m ----> <--------- k
+ a - b - c - d - e - f   |
+            |        |  |
+            i - h - [g] <------ Where the slow and fast pointer meet.
+  Cycle is n long.
+
+Distance travelled by fast pointer => df = m + C1 * n + k
+Distance travelled by slow pointer => ds = m + C2 * n + k
+Distance travelled by fast pointer is twice the distance travelled by slow pointer => df = 2 * ds
+
+m + C1 * n + k = 2 * (m + C2 * n + k)
+m + k = (C1 - 2 * C2) * n
+m = C * n - k
+So the new pointer and the old slow pointer will travel the same distance to hit the start of the cycle.
+
 This question is extra bullshit.
 */
 
